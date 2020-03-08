1796a8cb78fa6221ce2fa599d3a74dcf2c4aec45
Alessandro Valeri (alenaksu) <misterroot@gmail.com>
2020-03-09 00:51:00 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -4,106 +4,19 @@ import { getType, isPrimitive, JsonObject, generateNodePreview, isNode, classNam
 
 import styles from 'bundle-text:./styles.css';
 
-const ObjectKey = ({ isCollapsable, collapsed, onClick, key }) => html`
-    <span
-        class=${classNames(key && 'key', isCollapsable && 'collapsable', collapsed && 'collapsableCollapsed')}
-        onClick=${onClick}
-    >
-        ${key}:
-    </span>
-`;
-
-class JsonNestedObjectNode extends TinyElement {
-    data = null;
-    collapsed = true;
-
-    static get properties() {
-        return {
-            data: JsonObject,
-            collapsed: Boolean,
-            key: String
-        };
+const toggleNode = path => state => ({
+    ...state,
+    expanded: {
+        ...state.expanded,
+        [path]: !state.expanded[path]
     }
-
-    static get is() {
-        return 'json-nested-object-node';
-    }
-
-    handleKeyClick = e => {
-        e.preventDefault();
-        this.collapsed = !this.collapsed;
-    };
-
-    renderValue(node) {
-        return isNode(node)
-            ? node
-            : html`
-                  <span class=${getType(node)}>${JSON.stringify(node)}</span>
-              `;
-    }
-
-    renderChild(node) {
-        return this.collapsed
-            ? html`
-                  <span class="preview">
-                      ${generateNodePreview(node)}
-                  </span>
-              `
-            : html`
-                  <json-object-node data=${node}></json-object-node>
-              `;
-    }
-
-    render() {
-        const { data, key } = this;
-        const isPrimitiveOrNode = isPrimitive(data) || isNode(data);
-
-        return html`
-            ${ObjectKey({
-                isCollapsable: !isPrimitiveOrNode,
-                collapsed: this.collapsed,
-                key,
-                onClick: !isPrimitiveOrNode && this.handleKeyClick
-            })}
-            ${isPrimitiveOrNode ? this.renderValue(data) : this.renderChild(data)}
-        `;
-    }
-}
-
-class JsonObjectNode extends TinyElement {
-    data = null;
-    collapsed = true;
-
-    static get is() {
-        return 'json-object-node';
-    }
-
-    static get properties() {
-        return {
-            data: JsonObject,
-            collapsed: Boolean
-        };
-    }
-
-    render() {
-        const { data } = this;
-
-        return html`
-            <ul>
-                ${Object.keys(data).map(key =>
-                    html`
-                        <li>
-                            <json-nested-object-node key=${key} data=${data[key]}></json-nested-object-node>
-                        </li>
-                    `.withKey(key)
-                )}
-            </ul>
-        `;
-    }
-}
+});
 
 class JsonViewer extends TinyElement {
     data = null;
+    state = {
+        expanded: {}
+    };
 
     static get is() {
         return 'json-viewer';
@@ -114,6 +27,12 @@ class JsonViewer extends TinyElement {
             data: JsonObject
         };
     }
+
+    handleKeyClick = path => e => {
+        e.preventDefault();
+
+        this.setState(toggleNode(path));
+    };
 
     connectedCallback() {
         if (!this.hasAttribute('data')) {
@@ -126,18 +45,57 @@ class JsonViewer extends TinyElement {
         super.connectedCallback();
     }
 
-    renderObjectNode(data) {
+    renderObjectNode(data, path = '') {
         return html`
             <ul>
-                ${Object.keys(data || {}).map(key =>
-                    html`
+                ${Object.keys(data || {}).map(key => {
+                    const nodeData = data[key];
+                    const isPrimitiveOrNode = isPrimitive(nodeData) || isNode(nodeData);
+                    const nodePath = path ? `${path}.${key}` : key;
+
+                    return html`
                         <li>
-                            <json-nested-object-node key=${key} data=${data[key]}></json-nested-object-node>
+                            ${this.renderObjectKey({
+                                isCollapsable: !isPrimitiveOrNode,
+                                collapsed: !this.state.expanded[nodePath],
+                                key,
+                                onClick: !isPrimitiveOrNode && this.handleKeyClick(nodePath)
+                            })}
+                            ${isPrimitiveOrNode ? this.renderValue(nodeData) : this.renderChild(nodeData, nodePath)}
                         </li>
-                    `.withKey(key)
-                )}
+                    `.withKey(key);
+                })}
             </ul>
         `;
+    }
+
+    renderObjectKey({ isCollapsable, collapsed, onClick, key }) {
+        return html`
+            <span
+                class=${classNames(key && 'key', isCollapsable && 'collapsable', collapsed && 'collapsableCollapsed')}
+                onClick=${onClick}
+            >
+                ${key}:
+            </span>
+        `;
+    }
+
+    renderValue(node) {
+        return isNode(node)
+            ? node
+            : html`
+                  <span class=${getType(node)}>${JSON.stringify(node)}</span>
+              `;
+    }
+
+    renderChild(node, path) {
+        return !this.state.expanded[path]
+            ? html`
+                  <span class="preview">
+                      ${generateNodePreview(node)}
+                  </span>
+              `
+            : this.renderObjectNode(node, path);
     }
 
     render() {
@@ -145,10 +103,9 @@ class JsonViewer extends TinyElement {
             <style>
                 ${styles}
             </style>
+
             ${this.renderObjectNode(this.data)}
         `;
     }
 }
-customElements.define(JsonObjectNode.is, JsonObjectNode);
-customElements.define(JsonNestedObjectNode.is, JsonNestedObjectNode);
 customElements.define(JsonViewer.is, JsonViewer);
